b49f3b50e2bf82f3868705ae48d08751342f6e71
Prince Gupta <kunalgupta1354@gmail.com>
2025-07-25 17:49:27 +0530

    Update auth.js

--- route/auth.js
+++ route/auth.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {sendOtp, verifyOtp, register, login, profile, forgotPassword, resetPassword} = require('../controllers/authController');
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post('/send-otp', sendOtp);
 router.post('/verify-otp', verifyOtp);
 router.post('/register', register);
 router.post('/login', login);
-router.get("/profile", profile);
+router.get("/profile",authMiddleware, profile);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
 
