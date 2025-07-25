8038b10325aadbaeed72f2b134bf94ab710d8fd8
Prince Gupta <kunalgupta1354@gmail.com>
2025-07-25 17:34:10 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -106,6 +106,18 @@ exports.login =  async (req, res) => {
     res.json({ token });
 };
 
+//  Profile route
+exports.profile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (error) {
+    console.error("Error fetching profile:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 
 // Forgot Password Route
 exports.forgotPassword = async (req, res) => {
