b46159d4313a91c23c272596ba26ffdbfdeba04a
Prince Gupta <kunalgupta1354@gmail.com>
2025-07-25 23:50:04 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -8,7 +8,10 @@ const authRoutes = require("./route/auth");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://app.netlify.com',
+  credenatials: true,
+}));
 app.use(express.json());
 
 app.use("/api", authRoutes);
